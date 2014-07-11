fd0d047bc663cc82fade5238d58c5a14761ee9be
natSim27 <natjsimons@hotmail.com>
2014-07-11 01:07:51 -0400

    Signal Beacon Custom View

--- app/src/main/java/ca/mixitmedia/ghostcatcher/app/Tools/Amplifier.java
+++ app/src/main/java/ca/mixitmedia/ghostcatcher/app/Tools/Amplifier.java
@@ -5,15 +5,19 @@ import android.media.AudioManager;
 import android.media.SoundPool;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 
 import java.io.File;
+import java.sql.Time;
+import java.util.Calendar;
 
 import ca.mixitmedia.ghostcatcher.app.R;
 import ca.mixitmedia.ghostcatcher.experience.gcEngine;
+import ca.mixitmedia.ghostcatcher.views.SignalBeaconView;
 
 public class Amplifier extends ToolFragment {
 
@@ -24,6 +28,24 @@ public class Amplifier extends ToolFragment {
 
         super.onCreateView(inflater, container, savedInstanceState);
         View view = inflater.inflate(R.layout.tool_amplifier, container, false);
+
+        final SignalBeaconView beaconView = (SignalBeaconView) view.findViewById(R.id.signal_beacon_wave);
+
+        beaconView.setWaveFunction(new SignalBeaconView.WaveFunction() {
+            @Override
+            public float getGraphYWaveOne(float graphX, float amplitude) {
+                float period = 100;
+                return amplitude + (float) ((amplitude/1.5) * Math.sin(graphX / period + (Calendar.getInstance().get(Calendar.SECOND))));
+            }
+            public float getGraphYWaveTwo(float graphX, float amplitude) {
+                float period = 50;
+                return amplitude + (float) ((amplitude/2) * Math.cos(graphX / period));
+            }
+            public float getGraphYWaveThree(float graphX, float amplitude) {
+                float period = 80;
+                return amplitude + (float) ((amplitude/2) * Math.sin(graphX/period));
+            }
+        });
 
         ImageView overlay = (ImageView) view.findViewById(R.id.overlay);
         overlay.setImageURI(rootUri.buildUpon().appendPath("skins").appendPath("amplifier").appendPath("amplifier_overlay.png").build());
--- app/src/main/java/ca/mixitmedia/ghostcatcher/views/SignalBeaconView.java
+++ app/src/main/java/ca/mixitmedia/ghostcatcher/views/SignalBeaconView.java
@@ -5,6 +5,9 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.util.AttributeSet;
+import android.util.Log;
+import android.view.Surface;
+import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
 /**
@@ -13,16 +16,62 @@ import android.view.SurfaceView;
 public class SignalBeaconView extends SurfaceView {
 
     Paint waveOne, waveTwo,waveThree;
-    float graphX, graphY;
     WaveFunction waveFunction;
+    SurfaceThread surfaceThread;
+    int width, height;
 
     public SignalBeaconView(Context context, AttributeSet attr){
         super(context, attr);
 
         waveOne = new Paint();
-        waveTwo = new Paint();
-        waveThree = new Paint();
+        waveOne.setColor(Color.GREEN);
+        waveOne.setStrokeWidth(7);
 
+        waveTwo = new Paint();
+        waveTwo.setColor(Color.MAGENTA);
+        waveTwo.setStrokeWidth(7);
+
+        waveThree = new Paint();
+        waveThree.setColor(Color.YELLOW);
+        waveThree.setStrokeWidth(7);
+
+        surfaceThread = new SurfaceThread(getHolder(), this);
+
+        getHolder().addCallback(new SurfaceHolder.Callback() {
+            @Override
+            public void surfaceCreated(SurfaceHolder holder) {
+                surfaceThread.setEnable(true);
+                surfaceThread.start();
+            }
+
+            @Override
+            public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+
+            }
+
+            @Override
+            public void surfaceDestroyed(SurfaceHolder holder) {
+                Boolean hasJoined = false;
+                surfaceThread.setEnable(false);
+
+                while (hasJoined == false) {
+                    try {
+                        surfaceThread.join();
+                        hasJoined = true;
+                    } catch (java.lang.InterruptedException e) {
+                    }
+
+                }
+            }
+        });
+
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        width = getMeasuredWidth();
+        height = getMeasuredHeight();
     }
 
     @Override
@@ -33,23 +82,80 @@ public class SignalBeaconView extends SurfaceView {
 
     public void drawWavePoints(Canvas canvas){
         canvas.drawColor(Color.BLACK);
-        float[] waveOnePointsArray = new float[(this.getWidth()*2)];
+        float[] waveOnePointsArray = new float[width*2];
+        float[] waveTwoPointsArray = new float[width*2];
+        float[] waveThreePointsArray = new float[width*2];
 
         for(int i=0; i < this.getWidth(); i++){
             waveOnePointsArray[i*2]=i;
-            waveOnePointsArray[(i*2)+1] = waveFunction.getGraphY(i);
+            waveTwoPointsArray[i*2]=i;
+            waveThreePointsArray[i*2]=i;
+
+            if(waveFunction == null){
+                waveOnePointsArray[(i*2)+1] = 1;
+                waveTwoPointsArray[(i*2)+1] = 1;
+                waveThreePointsArray[(i*2)+1] = 1;
+            }
+            else{
+                waveOnePointsArray[(i*2)+1] = waveFunction.getGraphYWaveOne(i,((float) height/2));
+                waveTwoPointsArray[(i*2)+1] = waveFunction.getGraphYWaveTwo(i,((float) height/2));
+                waveThreePointsArray[(i*2)+1] = waveFunction.getGraphYWaveThree(i,((float) height/2));
+            }
         }
 
         canvas.drawPoints(waveOnePointsArray, waveOne);
+        canvas.drawPoints(waveTwoPointsArray, waveTwo);
+        canvas.drawPoints(waveThreePointsArray, waveThree);
     }
 
     public interface WaveFunction{
-        public float getGraphY(float graphX);
+        public float getGraphYWaveOne(float graphX, float amplitude);
+        public float getGraphYWaveTwo(float graphX, float amplitude);
+        public float getGraphYWaveThree(float graphX, float amplitude);
     }
 
     public void setWaveFunction(WaveFunction waveFunction){
         this.waveFunction = waveFunction;
     }
 
-
 }
+
+class SurfaceThread extends Thread{
+    SurfaceHolder surfaceHolder;
+    SignalBeaconView beaconView;
+    Boolean enable = false;
+
+
+    public SurfaceThread(SurfaceHolder holder, SignalBeaconView bView){
+        surfaceHolder = holder;
+        beaconView = bView;
+
+    }
+
+    public void setEnable(Boolean enable) {
+        this.enable = enable;
+    }
+
+    @Override
+    public void run() {
+        Canvas canvas = null;
+
+        while(enable == true) {
+            try {
+                canvas = surfaceHolder.lockCanvas();
+                if(canvas != null){
+                    synchronized (surfaceHolder){
+                        beaconView.drawWavePoints(canvas);
+                    }
+                }
+
+            }
+            finally {
+                if(canvas != null)
+                    surfaceHolder.unlockCanvasAndPost(canvas);
+            }
+
+        }
+    }
+}
+
